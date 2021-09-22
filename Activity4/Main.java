ac75489bea41be0a00ad519d2e12483cef09b085
Your Name <olin23@sjs.org>
2021-09-21 19:39:17 -0500

    Edited Main.java for a third time

--- Activity4/Main.java
+++ Activity4/Main.java
@@ -1,15 +1,8 @@
 public class Main{
-<<<<<<< HEAD
     public static void main(String[] arg){
         System.out.println("Oliver");
         System.out.println("Lin");
         System.out.println("olin23@sjs.org");
+        System.out.println("This is PDS!!");
     }
 }
-=======
-  public static void main (String[] args){
-    System.out.println("Oliver");
-    Systen.out.println("Lin");
-  }
-}
->>>>>>> development
